b8925ef7e9cbd3d39be3a93319da6436ce7522f4
Nicolas Ariza <nicolas_ariza@outlook.com>
2022-08-17 00:08:45 -0500

    Indexes added

--- src/events/entities/events.entity.ts
+++ src/events/entities/events.entity.ts
@@ -1,5 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * To help speed up this search, we can define an index on the “name” column
+ * using the @Index decorator.
+ */
+// Composite index that contains Multiple columns
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
@@ -8,6 +14,7 @@ export class Event {
   @Column()
   type: string;
 
+  @Index()
   @Column()
   name: string;
 
